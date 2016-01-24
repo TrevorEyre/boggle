2225e94f9f22b9b12e0fafc590d13f6f3842084c
shwet <shwet@pdx.edu>
2016-01-23 16:28:11 -0800

    added a countdown timer running for 3 minutes.

--- app/src/main/java/com/team1/cs410/boggle/GameBoard.java
+++ app/src/main/java/com/team1/cs410/boggle/GameBoard.java
@@ -1,7 +1,9 @@
 package com.team1.cs410.boggle;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,10 +12,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class GameBoard extends AppCompatActivity {
 
+
+    private CountDownTimer countDownTimer;
+    private boolean timerHasStarted = false;
+    public TextView text;
+    private final long startTime = 180 * 1000;
+    private final long interval = 1*1000;
     //Button disabledbuttons[];
     ArrayList<Button> disabledbuttons;
     @Override
@@ -22,6 +32,18 @@ public class GameBoard extends AppCompatActivity {
         setContentView(R.layout.activity_game_board);
         disabledbuttons = new ArrayList<>();
         Intent intent = getIntent();
+        text = (TextView)this.findViewById(R.id.timer);
+        countDownTimer = new MyCountDownTimer(startTime, interval);
+        text.setText(text.getText() + String.valueOf(startTime/1000));
+        if(!timerHasStarted){
+            countDownTimer.start();
+            timerHasStarted = true;
+        }
+        else
+        {
+            countDownTimer.cancel();
+            timerHasStarted = false;
+        }
     }
 
     //handle the click on board buttons
@@ -49,5 +71,37 @@ public class GameBoard extends AppCompatActivity {
         disabledbuttons.clear();
     }
 
+    //@Override
+    public void onClick(View v)
+    {
+      if(!timerHasStarted){
+          countDownTimer.start();
+          timerHasStarted = true;
+      }
+        else
+      {
+          countDownTimer.cancel();
+          timerHasStarted = false;
+      }
+    }
+    public class MyCountDownTimer extends CountDownTimer{
+        public MyCountDownTimer(long startTime, long interval){
+            super(startTime, interval);
+        }
+
+        public void onFinish(){
+            text.setText("Times up!");
+        }
+        public void onTick(long millsUntilFinished){
+            if(millsUntilFinished/1000 == 30) {
+                text.setTextColor(Color.RED);
+            }
+            text.setText("" + String.format("%02d",((millsUntilFinished/1000)/60))+":"+String.format("%02d",((millsUntilFinished/1000)%60)));
+        }
+
+            /*long remainedSecs = millsUntilFinished/1000;
+            text.setText("" + millsUntilFinished/1000);*/
+
+    }
 
 }
