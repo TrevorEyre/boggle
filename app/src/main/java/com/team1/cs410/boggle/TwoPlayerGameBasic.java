2bdafd08a0c7ffcaa8552c417aec30bd059e6839
shwet <shwet@pdx.edu>
2016-02-21 13:41:26 -0800

    added a code for wrapper board

--- app/src/main/java/com/team1/cs410/boggle/TwoPlayerGameBasic.java
+++ app/src/main/java/com/team1/cs410/boggle/TwoPlayerGameBasic.java
@@ -1,22 +1,30 @@
 package com.team1.cs410.boggle;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class TwoPlayerGameBasic extends AppCompatActivity {
     private CountDownTimer countDownTimer;
     private boolean timerHasStarted = false;
     public TextView text;
+    private Activity activity;
+    private Game game;
     private final long startTime = 180 * 1000;
     private final long interval = 1*1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_player_game_basic);
+        activity = this;
+        game = new Game(this, this);
+        LinearLayout gameBoardWrapper = (LinearLayout)findViewById(R.id.game_board_wrapper);
+        gameBoardWrapper.addView(game.getBoard());
         text = (TextView)this.findViewById(R.id.timer);
         countDownTimer = new TwoPlayerGameBasic.MyCountDownTimer(startTime, interval);
         text.setText(text.getText() + String.valueOf(startTime/1000));
